273e9e7b95873b9ea4f089db9a9f7453f7a540ef
RamSaravanaRaja <rramsaravana@gmail.com>
2021-06-23 14:34:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// imports
+
+
+
+
+// initialisations
+let before_email = document.getElementById(before-mail);
+let email_subject = document.getElementById(email-head);
+let email_section = document.getElementById(mail-body);
+
+
+
+
+// functions called from hardcode
+composeMail = () =>{
+    
+
+}
+
+inboxMails = () =>{
+    
+}
+
+draftMails = () =>{
+    
+}
+
+sentMails = () =>{
+    
+}
+
+trash = () =>{
+    
+}
+
+
+
+
+// API related
